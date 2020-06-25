223533c452a80b490d3d093d9b15693aa731a8e4
Shawrex <38359132+Shawrex@users.noreply.github.com>
2020-06-25 11:53:00 +0200

    test 3

---
+++ TestGit/Assets/Scripts/CubeRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class CubeRotate : MonoBehaviour
+{
+    /*
+    J'vais essayer de m'adapter à ton style de code ou de game design
+    Parce que sinon on risque de faire des truc un peu bordéliques
+    */
+
+    private Rigidbody rb;
+
+    private void Start() => rb = GetComponent<Rigidbody>();
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(7.5f, 15f, 7.5f) * Time.deltaTime);
+    }
+}
